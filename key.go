7117f8c02258426fdeca5c24ea531272344969a1
hissec <Hissec@163.com>
2022-12-31 21:03:11 +0800

    增加加密方法

--- key.go
+++ key.go
@@ -2,15 +2,24 @@ package Hissec
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
 	"fmt"
 	"math/rand"
 	"strconv"
 	"time"
 )
 
+//GetMd5Key  生成随机的MD5
 func GetMd5Key() string {
 	time.Sleep(time.Microsecond)
 	rand.Seed(time.Now().UnixNano())
 	key := md5.Sum([]byte(strconv.Itoa(rand.Int())))
 	return fmt.Sprintf("%x", key)
 }
+
+// GetVerifyCode 密码算法
+func GetVerifyCode(value string) string {
+	key := sha256.Sum256([]byte(value))
+	return fmt.Sprintf("%x", key)
+}
+
